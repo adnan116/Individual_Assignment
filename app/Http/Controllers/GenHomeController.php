c6878d746dc37f10bc62766c5f0846fc32314476
adnan116 <khandakar.adnan21@gmail.com>
2020-04-11 20:45:52 +0600

    posts details added in general home page

--- app/Http/Controllers/GenHomeController.php
+++ app/Http/Controllers/GenHomeController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class GenHomeController extends Controller
 {
     public function index(){
+
+    	$place = DB::select("select posts.poid, places.pname, places.pdes, countries.name, posts.cost, posts.travel_medium, posts.facilities from posts, places, countries where posts.pid = places.pid and posts.status=1");
     	
-    	return view('genHome.index');
+    	return view('genHome.index', ['places'=>$place]);
     }
+
 }
--- resources/views/genHome/index.blade.php
+++ resources/views/genHome/index.blade.php
@@ -8,6 +8,31 @@
 		<h1>Welcome!!! ABC Travel Guide</h1><br><br>
 		<a href="/login">Login</a><br><br>
 		<a href="/register">Registration</a>
+
+		<br><br><br>
+		<h3>Post About Travelling Places</h3>
+		<table border="1">
+			<tr>
+				<th>Place Name</th>
+				<th>Place Description</th>
+				<th>Country Name</th>
+				<th>Travel Cost</th>
+				<th>Travel Medium</th>
+				<th>Facilities</th>
+			</tr>
+			
+			@foreach($places as $p)
+			<tr>
+				<td>{{$p->pname}}</td>
+				<td>{{$p->pdes}}</td>
+				<td>{{$p->name}}</td>
+				<td>{{$p->cost}}</td>
+				<td>{{$p->travel_medium}}</td>
+				<td>{{$p->facilities}}</td>
+			</tr>
+			@endforeach
+
+		</table>
 	</center>
 
 
